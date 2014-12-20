14ba348ab05fd33ea3c97a8c21333b30f8dcb560
rainbee2214 <rainbee2214@gmail.com>
2014-12-20 14:31:20 -0400

    Project clean up.

---
+++ ThePatternGame/Assets/Scripts/PresentDropper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresentDropper : MonoBehaviour 
+{
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
---
+++ ThePatternGame/Assets/Scripts/SleighRideController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleighRideController : MonoBehaviour 
+{
+    public static float poolSize = 20;
+    public static float dropDelay = 1f;
+    public static float gravityLowerBound = 0.01f;
+    public static float gravityUpperBound = 0.1f;
+
+    public static SleighRideController controller;
+
+    public int score;
+    public int level;
+
+	void Start () 
+    {
+        controller = this;
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    void LevelUp()
+    {
+        dropDelay -= 0.1f;
+        gravityLowerBound += 0.01f;
+        gravityUpperBound += 0.01f;
+    }
+}
